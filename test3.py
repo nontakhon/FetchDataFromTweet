26f33752092ba67451f53abdc550df864c763684
nontakho <nontakho@gmail.com>
2017-11-20 16:27:18 +0700

    temp

--- test3.py
+++ test3.py
@@ -1,3 +1,5 @@
+import os
+import io
 import tweepy
 from tweepy import OAuthHandler
  
@@ -9,12 +11,29 @@ access_secret = 'QoQL9w6I44Eb3DmmPQAVgdmzpo1vLkVRfWqBvbujxBrwE'
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 api = tweepy.API(auth)
-
 path="C:\\Users\\MicroComSci\\Documents\\FetchDataFromTweet\\tweet"
+countline=1
 
 #for status in tweepy.Cursor(api.home_timeline).items(10):
 for status in tweepy.Cursor(api.user_timeline, id="nontakhon").items(10):
-    # Process a single status
-    print(status.text + " "+status.created_at)
+   #str dateToString=print(" "+status.created_at)
+    #dateOnly=dateToString
+    stringDate=status.created_at.strftime('%d%m%Y')
+    pathfile=createdFolder(path,folder)+countline
+    day_file=open(pathfile,'a')
+    day_file.write(stringDate+"  "+status.text)
+    print(a)
     
+
+
+def createdFolder(path,folder):
+    folderCreateCheck=path+"\\"+folde
+    if  not os.path.exists(folderCreateCheck):
+    os.makedirs(folderCreateCheck)
+    SetNewCount()
+    print("OK,Creaated"+folderCreateCheck)
+    return folderCreateCheck
+
+def SetNewCount():
+    countline=1
     
