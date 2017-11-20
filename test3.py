635d305a4877efe28c5cdd26aa9c6ee43ab398f0
nontakho <nontakho@gmail.com>
2017-11-20 17:16:03 +0700

    complete minning frome twitter

--- test3.py
+++ test3.py
@@ -3,6 +3,7 @@ import io
 import tweepy
 from tweepy import OAuthHandler
  
+
 consumer_key = '3eRdmwDRW7ezS0ysg51i8I2yx'
 consumer_secret = 'x4LT2G2otn7UYBG6alP2P7tSBNJ0HILUJHUbSFmhswI5BX7qHf'
 access_token = '2661873764-42So7jeZ5mrPYNwWovPfrrj6LPVvabm7peeQEcl'
@@ -12,28 +13,45 @@ auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 api = tweepy.API(auth)
 path="C:\\Users\\MicroComSci\\Documents\\FetchDataFromTweet\\tweet"
-countline=1
+countline = 1 
+countfile = 1
+
+def createdFolder(path,stringDate):
+    folderCreateCheck=path+"\\"+stringDate
+    if  not os.path.exists(folderCreateCheck):
+        os.makedirs(folderCreateCheck)
+        setNewCount()
+        print("OK,Creaated"+folderCreateCheck)
+    return folderCreateCheck
+
+def setNewCount():
+    global countline
+    global countfile
+    countline=1
+    countfile=1
+
+def checkCountline():
+    global countline
+    global countline
+    global countfile
+    if (countline > 100) :
+        countline=1
+        countfile+=1
+         
 
 #for status in tweepy.Cursor(api.home_timeline).items(10):
 for status in tweepy.Cursor(api.user_timeline, id="nontakhon").items(10):
    #str dateToString=print(" "+status.created_at)
     #dateOnly=dateToString
     stringDate=status.created_at.strftime('%d%m%Y')
-    pathfile=createdFolder(path,folder)+countline
-    day_file=open(pathfile,'a')
-    day_file.write(stringDate+"  "+status.text)
-    print(a)
-    
+    checkCountline()
+    pathfile=createdFolder(path,stringDate)+"\\"+stringDate+"_"+str(countfile)+".txt"
+    countline+=1
+
+    filetext=open(pathfile,'a')
+    filetext.write(stringDate+"  "+status.text) #2 space
+    filetext.write("\n")
 
 
-def createdFolder(path,folder):
-    folderCreateCheck=path+"\\"+folde
-    if  not os.path.exists(folderCreateCheck):
-    os.makedirs(folderCreateCheck)
-    SetNewCount()
-    print("OK,Creaated"+folderCreateCheck)
-    return folderCreateCheck
 
-def SetNewCount():
-    countline=1
     
